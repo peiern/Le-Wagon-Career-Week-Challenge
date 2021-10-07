95bc59af4ee3e34648a68269730953dd4f39f778
Pei Ern Koh <peiern.28@gmail.com>
2021-10-07 17:48:22 +0800

    fixed footer css

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const url = "https://teclead.de/recruiting/radios";
-const stations = document.querySelector(".stations");
-const nowPlaying = document.querySelector(".now-playing");
+const url = 'https://teclead.de/recruiting/radios';
+const stations = document.querySelector('.stations');
+const nowPlaying = document.querySelector('.now-playing');
 
 fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    data.radios.map((radio) => {
-      const radioDetails = `
-      <div class="radio">
-        ${radio.name}
-        <p><strong>${radio.frequency}</strong></p>
-      </div>
-      <div class="image hide">
-        <i class="fas fa-minus-circle"></i>
-        <img src="${radio.image}">
-        <i class="fas fa-plus-circle"></i>
-      </div>`;
+.then(res => res.json())
+.then((data) => {
 
-      const playing = `
-      <div class="footer hide">
-        <h6>CURRENTLY PLAYING</h6>
-        <p>${radio.name}</p>
-      </div>`;
+  data.radios.map(radio => {
+    // <--- array [0] --->
+    // radioName = data.radios[0].name;
+    // radioFrequency = data.radios[0].frequency;
+    // radioImage = data.radios[0].image;
 
-      stations.insertAdjacentHTML("beforeend", radioDetails);
-      nowPlaying.insertAdjacentHTML("beforeend", playing);
-    });
+    // const radioOneDetails = document.querySelector('#station-1');
+    // radioOneDetails.innerHTML = `${radioName} <strong>${radioFrequency}</strong>`;
 
-    // addEventListener on click
-    // on click, show image of clicked radio
-    // hide the rest
-    // show footer of clicked radio (if same radio then show, else hide)
+    const radioDetails = `
+    <div class="radio">
+      ${radio.name}
+      <p><strong>${radio.frequency}</strong></p>
+    </div>
+    <div class="image hide">
+      <i class="fas fa-minus-circle"></i>
+      <img src="${radio.image}">
+      <i class="fas fa-plus-circle"></i>
+    </div>`;
 
-    const selecting = document.querySelector(".radio");
+    const playing = `
+    <div class="footer hide">
+      <h6>CURRENTLY PLAYING</h6>
+      <p>${radio.name}</p>
+    </div>`;
+    stations.insertAdjacentHTML("beforeend", radioDetails);
+    nowPlaying.insertAdjacentHTML("beforeend", playing);
+  });
+
+  // addEventListener on click
+  // on click, show image of clicked radio
+  // hide the rest
+  // show footer of clicked radio (if same radio then show, else hide)
+
+  const selecting = document.querySelector(".radio");
     selecting.addEventListener("click", (event) => {
-        const img = document.querySelector(".image");
-        const foot = document.querySelector(".footer");
-        img.classList.toggle("hide");
-        foot.classList.toggle("hide");
-      // if (event.currentTarget.innerHTML === selecting) {
-      //   img.classList.remove("hide");
-      // } else {
-      //   img.classList.add("hide");
-      // }
-      // console.log("hello");
+      const img = document.querySelector(".image");
+      const foot = document.querySelector(".footer");
+      img.classList.toggle("hide");
+      foot.classList.toggle("hide");
+
     });
+
   });
