4888a8edd677046d560f78b4a4118b687060e2ee
Pei Ern Koh <peiern.28@gmail.com>
2021-10-13 16:53:34 +0800

    able show footer details

--- index.js
+++ index.js
@@ -5,58 +5,43 @@ fetch(url)
 .then(res => res.json())
 .then((data) => {
 
-  data.radios.map(radio => {
-    // <--- array [0] --->
-    // radioName = data.radios[0].name;
-    // radioFrequency = data.radios[0].frequency;
-    // radioImage = data.radios[0].image;
-
-    // const radioOneDetails = document.querySelector('#station-1');
-    // radioOneDetails.innerHTML = `${radioName} <strong>${radioFrequency}</strong>`;
-
+  data.radios.forEach(radio => {
     const radioDetails = `
-    <div class="radio">
       <div class="image hide">
         <i class="fas fa-minus-circle"></i>
         <img src="${radio.image}">
         <i class="fas fa-plus-circle"></i>
       </div>
-      ${radio.name}
-      <p><strong>${radio.frequency} FM</strong></p>
-    </div>`;
+      <div class="radio">
+        <p class="radio-name">${radio.name}</p>
+        <p><strong>${radio.frequency} FM</strong></p>
+      </div>`;
     stations.insertAdjacentHTML("beforeend", radioDetails);
   });
 
-  // addEventListener on click
-  // on click, show image of clicked radio
   // hide the rest
   // show footer of clicked radio (if same radio then show, else hide)
 
-  const radioStations = Array.from(document.querySelectorAll(".radio"));
+  const showDetails = (event) => {
+    const allRadioImage = document.querySelectorAll(".image");
+    allRadioImage.forEach((radioImage) => {
+      radioImage.classList.add("hide");
+    });
 
-  const resetImg = () => {
-    radioStations.forEach(radioStation => {
-      const img = radioStation.querySelector(".image");
-      img.classList.add("hide");
-    })
+    const radioStation = event.currentTarget.previousElementSibling;
+    radioStation.classList.remove("hide");
+
+    const footerDetails = document.querySelector(".footer-details");
+    footerDetails.classList.remove("hide");
+
+    const footerStation = event.currentTarget.querySelector(".radio-name").innerText;
+    const footerStationName = footerDetails.querySelector(".footer-radio-name");
+    footerStationName.innerText = footerStation;
   };
 
-  radioStations.forEach(radioStation => {
-    radioStation.addEventListener("click", (event) => {
-
-      resetImg()
-      const img = radioStation.querySelector(".image");
-      img.classList.remove("hide");
-
-      const footerStation = document.querySelector(".footer p");
-      footerStation.innerHTML =  `${radio.name}`;
-
-      // get the current station
-      // const currentStation = "test"
-
-      // // update html in footer
-      // footerStation.innerHTML = currentStation
+  // addEventListener on click
+  const allRadios = document.querySelectorAll(".radio");
+    allRadios.forEach(radio => {
+    radio.addEventListener("click", showDetails);
     });
-  })
-
 });
